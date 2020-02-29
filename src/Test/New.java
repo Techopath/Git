68094143fb337e10bb8728aaa0f0ba043c14c5ce
Techopath <58576711+Techopath@users.noreply.github.com>
2020-02-29 16:18:18 -0500

    Delete New.java

--- src/Test/New.java
+++
@@ -1,9 +0,0 @@
-package Test;
-
-public class New {
-    public static void main(String[] args) {
-        //practice for pushing and pulling
-        
-        //A friend made some changes to the code and put it into GitHub, then I pulled it from GitHub
-    }
-}
