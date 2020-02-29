2ceb1d5c7d20273494d0017cf64d2dfb34795b96
Muhammad <musadam28@gmail.com>
2020-02-29 10:50:15 -0500

    logout.java

---
+++ src/Test/Logout.java
@@ -0,0 +1,11 @@
+package Test;
+
+public class Logout {
+    public static void main(String[] args) {
+        int i=0;
+        while (i<10){
+            System.out.print("|"+ i +" | ");
+            i++;
+        }
+    }
+}
