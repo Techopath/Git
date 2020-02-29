8eaa3dcc0ba65821512fa35480f01359c014aa3f
Techopath <58576711+Techopath@users.noreply.github.com>
2020-02-29 16:05:56 -0500

    Update New.java

--- src/Test/New.java
+++ src/Test/New.java
@@ -3,5 +3,7 @@ package Test;
 public class New {
     public static void main(String[] args) {
         //practice for pushing and pulling
+        
+        //A friend made some changes to the code and put it into GitHub, then I pulled it from GitHub
     }
 }
