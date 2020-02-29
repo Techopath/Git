262e6761985e49a60767582ad247b04218a7cd11
Muhammad <musadam28@gmail.com>
2020-02-29 10:38:20 -0500

    add Login.java

---
+++ src/Test/Login.java
@@ -0,0 +1,14 @@
+package Test;
+
+public class Login { //type psvm (or main)to automatically write main method.
+    /*
+    * 1. git init
+    * 2. git status [will let you know if there is any update in your project like new data added or not]
+    * [ if no commits yet, it means you didn't upload anything to the cloud yet.]
+    * 3. git add -> */
+
+    public static void main(String[] args) {
+        System.out.println("Hello dear Human. You are here to learn a very important message sent to your inbox. Let's go and " +
+                "check it");
+    }
+}
